9f6de2894799b9c4adc04406b313d20074da085f
M. Feyza Yıldırım <144602340+Mfeyza@users.noreply.github.com>
2024-07-06 14:03:13 +0300

    fix:bug-responsive

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -38,82 +38,66 @@ const Login = () => {
         "The password must contain at least one special character (@$!%*?&)."
       ),
   });
-  const audio = new Audio('/open-and-closed-door-156814.mp3');
- 
+  const audio = new Audio("/open-and-closed-door-156814.mp3");
+
   return (
-    
-    <Container maxWidth="xxl" className="loginContainer">
-      <Grid
-        container
-        justifyContent="center"
-        direction="row-reverse"
-       
-      >
-        <Grid item xs={12} mb={3}>
-          <Typography variant="h3" color="primary" align="center" sx={{
-            marginTop:"4rem"
-          }}>Market Mate</Typography>
-        </Grid>
+    <Container className="loginContainer">
+      <Typography variant="h3" color="primary" align="center" sx={{}}>
+        Market Mate
+      </Typography>
+      <Box className="formimgBox" >
+      <img src={image} alt="img" className="boximg" />
+        <Formik
+          initialValues={{ email: "", password: "" }}
+          validationSchema={loginSchema}
+          onSubmit={(values, actions) => {
+            login(values);
+            actions.resetForm();
+            actions.setSubmitting(false);
+            audio.play();
+          }}
+        >
+          {(formikProps) => (
+            <>
+              <Form className="form">
+                <div className="imgLoginDiv">
+                  <img className="imgLogin" src={imgLogin} alt="" />
+                </div>
 
-        <Grid item xs={12} sm={10} md={6}sx={{
-          marginTop:"5rem",marginBottom:"5 rem"
-        }} >
-          <Formik
-            initialValues={{ email: "", password: "" }}
-            validationSchema={loginSchema}
-            onSubmit={(values, actions) => {
-              login(values)
-              actions.resetForm()
-              actions.setSubmitting(false)
-              audio.play()
-            }}
-          >  
-            {(formikProps) => (
-              <>
-                <Form className="form">
-                  <div className="imgLoginDiv">
-                  <img  className="imgLogin" src={imgLogin} alt=""/>
+                <Box>
+                  <div className="forminputs">
+                    <TextFieldRL
+                      formData={[
+                        { name: "email", label: "Email", type: "email" },
+                        {
+                          name: "password",
+                          label: "Password",
+                          type: "password",
+                        },
+                      ]}
+                      {...formikProps}
+                    />
+                    <Button
+                      variant="contained"
+                      type="submit"
+                      className="login-btn"
+                    >
+                      Submit
+                    </Button>
                   </div>
-                  
-
-                  <Box >
-                    <div className="forminputs">
-                      <TextFieldRL
-                        formData={[
-                          { name: "email", label: "Email", type: "email" },
-                          {
-                            name: "password",
-                            label: "Password",
-                            type: "password",
-                          },
-                        ]}
-                        {...formikProps}
-                      />
-                      <Button variant="contained" type="submit" className="login-btn">
-                        Submit
-                      </Button>
-                    </div>
-                    <Box sx={{ textAlign: "center", mt: 2 }}>
-            <Link className="link" to="/register">Do you have not an account?</Link>
-          </Box>
+                  <Box sx={{ textAlign: "center", mt: 2 }}>
+                    <Link className="link" to="/register">
+                      Do you have not an account?
+                    </Link>
                   </Box>
-                </Form>
-              </>
-            )}
-          
-          </Formik>
-         
-        </Grid>
+                </Box>
+              </Form>
+            </>
+          )}
+        </Formik>
 
-        <Grid item xs={10} sm={7} md={6}>
-          <Container>
-            <img src={image} alt="img" style={{
-              width:"150rem",
-              marginTop:"6rem"
-            }}/>
-          </Container>
-        </Grid>
-      </Grid>
+        
+      </Box>
     </Container>
   );
 };
